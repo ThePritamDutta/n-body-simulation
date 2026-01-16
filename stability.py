79af1c9e6f0b12c73b0ad98e2fdbdd8983544945
Pritam Dutta <126368703+ThePritamDutta@users.noreply.github.com>
2026-01-16 23:48:02 +0530

    Update stability.py

--- stability.py
+++ stability.py
@@ -1,18 +1,18 @@
 import numpy as np
 import integrators
 
-def stability(y,delta,method,y_old,t0,deriv,masses,radii,tf,h,contact_state):
+def stability(y,delta,method,y_old,t0,deriv,masses,radii,tf,h,contact_state,toler):
     y_pertubated = y_old + delta
     k = 0
     
     if (method =="rk45"):
-        k,t, y_new = integrators.rk45(deriv, t0, y_pertubated, tf, h, toler,masses,radii,k)
+        k,t, y_new,contact_state = integrators.rk45(deriv, t0, y_pertubated, tf, h, toler,masses,radii,contact_state,k)
     
     elif (method == "rk4"):
-        t, y_new,k = integrators.rk4(deriv, t0, y_pertubated, tf, h,masses,radii,k)
+        t, y_new,k,contact_state = integrators.rk4(deriv, t0, y_pertubated, tf, h,masses,radii,contact_state,k)
     
     elif (method == "verlet"):
-        t, y_new,k = integrators.verlet_step(t0, y_pertubated, masses, tf, h, radii, contact_state, k)
+        t, y_new,k,contact_state = integrators.verlet_step(t0, y_pertubated, masses, tf, h, radii, contact_state, k)
     
     delta_t = y_new - y   
 
@@ -22,11 +22,11 @@ def stability(y,delta,method,y_old,t0,deriv,masses,radii,tf,h,contact_state):
 
     lambda_t = (1.0/t[1:]) * np.log(normt/norm0)
 
-    print("Collisions in perturbed run:", k)
+    print("Collisions in perturbated run:", k)
     
     lambda_total = np.sum(lambda_t)*(1.0/tf)
     
-    print("2nd Simulation Done")
+    
 
     return lambda_total
     
